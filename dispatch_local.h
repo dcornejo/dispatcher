8d2c6ec7b511f48b52e1d558fb87a9eca7c815a9
Dave Cornejo <dave@dogwood.com>
2021-03-26 09:58:05 -1000

    user defined structure for args to handlers

---
+++ dispatch_local.h
@@ -0,0 +1,15 @@
+
+/*
+ * user modifiable structure of arguments passed to handler
+ * functions
+ */
+
+#ifndef DISPATCH_DISPATCH_LOCAL_H
+#define DISPATCH_DISPATCH_LOCAL_H
+
+struct _handler_args {
+    int x;
+};
+typedef struct _handler_args handler_args_t;
+
+#endif //DISPATCH_DISPATCH_LOCAL_H
